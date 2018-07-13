f77a35867643de72b2241f20e8a19ac80322ea86
Will Harris <william.harris@sonova.com>
2018-07-13 13:16:29 +0200

    Added Tages Anzeiger ad killing

--- kill-ads.js
+++ kill-ads.js
@@ -1,6 +1,6 @@
-function killAds() {
-    console.log('loop');
-    var ads = document.getElementsByTagName('wp-ad');
+function killAds(ads) {
+    // console.log('loop');
+    // console.log(ads);
     var ad, i;
     
     for (i = 0; i < ads.length; i++) {
@@ -9,4 +9,27 @@ function killAds() {
     }
 }
 
-window.setInterval(killAds, 500);
+function killWpAds() {
+    var ads = document.getElementsByTagName('wp-ad');
+    killAds(ads);
+}
+
+function killTaAds() {
+    //var ads = document.getElementsByTagName('iframe');
+    var ads1 = Array.from(document.querySelectorAll('[class^=ad-]'));
+    var ads2 = Array.from(document.querySelectorAll('[class^=publi]'));
+    var ads = ads1.concat(ads2);
+    killAds(ads);
+}
+
+var origin = window.location.origin;
+
+if (origin.indexOf('washingtonpost') >= 0) {
+    console.log('Killing Washington Post ads...');
+    window.setInterval(killWpAds, 500);
+} else if (origin.indexOf('tagesanzeiger') >= 0) {
+    console.log('Killing Tages Anzeiger ads...');
+    window.setInterval(killTaAds, 500);
+} else {
+    console.log('Unhandled site: ' + origin);
+}
