fa656624353558019a33427c21721c3f516c5040
Will Harris <will@harris.ch>
2018-10-22 13:55:01 +0200

    Added BBC, fixed logo and title for non-WP-only reality

--- kill-ads.js
+++ kill-ads.js
@@ -22,6 +22,14 @@ function killTaAds() {
     killAds(ads);
 }
 
+function killBbcAds() {
+    var ads1 = Array.from(document.getElementsByClassName('advert'));
+    var ads2 = Array.from(document.querySelectorAll('[class^=teads-inread]'));
+    var ads3 = Array.from(document.getElementsByClassName('bbccom_advert'));
+    var ads = ads1.concat(ads2).concat(ads3);
+    killAds(ads);
+}
+
 var origin = window.location.origin;
 
 if (origin.indexOf('washingtonpost') >= 0) {
@@ -30,6 +38,9 @@ if (origin.indexOf('washingtonpost') >= 0) {
 } else if (origin.indexOf('tagesanzeiger') >= 0) {
     console.log('Killing Tages Anzeiger ads...');
     window.setInterval(killTaAds, 500);
+} else if (origin.indexOf('bbc.co') >= 0) {
+    console.log('Killing BBC ads...');
+    window.setInterval(killBbcAds, 500);
 } else {
     console.log('Unhandled site: ' + origin);
 }
